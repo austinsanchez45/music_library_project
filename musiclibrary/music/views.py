6ba3e13075e0981d090fc432116b85df4d2b202a
austin sanchez <austinsanchez45@yahoo.com>
2021-11-08 09:37:56 -0600

    Created another path for urls and class and functions in views to add more functionality to the postman application.

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -9,13 +9,45 @@ from rest_framework import status
 class SongList(APIView):
 
     def get(self, request):
+        """Retrieve Object from Postman"""
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
         return Response(serializer.data)
 
     def post(self, request):
+        """Create new objects on Postman"""
         serializer = SongSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+   
+    def get_object(self, pk):
+        """Retrieves objects to be used in the following functions"""
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """Get song by id"""
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """Update song by id"""
+        song = self.get_object(pk)
+        serializer = SongSerializer(song, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """Delete song by id"""
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
